49c8bd537493411ad9941fdfc17802263d1444ed
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-03-04 14:15:06 +0100

    Make the timer async-compatible
    When the timer is started with the start_multi-method it will generate a
    new random id for that timer. The end_multi-method now takes this id as
    an argument to measure the time-span associated with that specific id.
    The start- and end-methods now use the same system, except that their
    timer has the fixed (and reserved) id of 0.

--- timer.py
+++ timer.py
@@ -1,27 +1,43 @@
 import time
+import random
+from sys import maxsize
+
+def _get_next_id():
+    return random.randint(1, maxsize) # 0 reserved
 
 class Timer:
     def __init__(self):
-        self.start_time = 0.0
         self.times = []
         self.total_cache_stale = True
         self.total_cache = 0.0
         
+        self.multi_start_times = {}
+    
     def start(self):
-        if self.start_time != 0.0:
-            raise RuntimeError("Timer not finished")
-        self.start_time = time.perf_counter()
-        return self.start_time
+        if 0 in self.multi_start_times:
+            raise RuntimeError("Timer already started")
+        self.multi_start_times[0] = time.perf_counter()
+        return self.multi_start_times[0]
         
     def end(self):
+        return self.end_multi(0)
+                              
+    def start_multi(self):
+        timer_id = _get_next_id()
+        self.multi_start_times[timer_id] = time.perf_counter()
+        return timer_id
+        
+    def end_multi(self, timer_id):
         end_time = time.perf_counter()
-        if self.start_time == 0.0:
-            raise RuntimeError("Timer not started")
-        amount = end_time-self.start_time
-        self.start_time = 0.0
+        if timer_id not in self.multi_start_times:
+            raise RuntimeError("Timer {} not started".format(timer_id))
+        amount = end_time - self.multi_start_times.pop(timer_id)
         self.times.append(amount)
         self.total_cache_stale = True
         return amount
+    
+    def get_running(self):
+        return list(self.multi_start_times.keys())
         
     def total(self):
         if self.total_cache_stale:
@@ -30,10 +46,11 @@ class Timer:
         return self.total_cache
     
     def reset(self):
-        self.start_time = 0.0
         self.times.clear()
         self.invocations = 0
         self.total_cache_stale = True
-    
+        
+        self.multi_start_times.clear()
+        
     
             
