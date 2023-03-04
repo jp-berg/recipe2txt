65128ee2de44c81fa393243728a68a3f4cc8f099
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-03-04 16:36:31 +0100

    Add type hints to rezepte.py

--- rezepte.py
+++ rezepte.py
@@ -2,32 +2,36 @@ import sys
 import os.path
 import validators
 from os import getcwd, makedirs
+import recipe_scrapers
 from recipe_scrapers import scrape_html
 from recipe_scrapers._exceptions import WebsiteNotImplementedError, NoSchemaFoundInWildMode, SchemaOrgException, ElementNotFoundInHtml
 from xdg import xdg_cache_home, xdg_data_home
 import aiohttp
 import asyncio
 import traceback
+from typing import Final, Callable, NewType, Tuple, Any
 
-program_name = "RezeptZuTXT"
-debug = True
-args_are_files = True
+program_name:Final[str] = "RezeptZuTXT"
+debug:Final[bool] = True
+args_are_files:Final[bool] = True
 
 #level 0 -> silent
 #level 1 -> errors
 #level 2 -> events
 #level 3 -> setup
+
+vlevel:int = -1
 if debug:
     vlevel = 3
 else:
     vlevel = 0
 
-def dprint(level, *args, sep=' ', end='\n', file=None, flush=False):
+def dprint(level:int, *args:str, sep:str=' ', end:str='\n', file=None, flush:bool=False) -> None:
     if level >= vlevel:
-        print(*arg, sep=sep, end=end, file=file, flush=flush)
+        print(*args, sep=sep, end=end, file=file, flush=flush)
     
 
-def ensure_existence_dir(*pathelements):
+def ensure_existence_dir(*pathelements:str) -> str:
     path = os.path.join(*pathelements)
     if path.startswith("~"):
         path = os.path.expanduser(path)
@@ -35,32 +39,35 @@ def ensure_existence_dir(*pathelements):
     os.makedirs(path, exist_ok=True)
     return path
 
-def ensure_existence_file(filename, *pathelements):
+def ensure_existence_file(filename:str, *pathelements:str) -> str:
     path = os.path.join(ensure_existence_dir(*pathelements), filename)
     if not os.path.isfile(path):
         with open(path, 'w') as file:
             pass
     return path
 
-known_urls_name = "knownURLs.txt"
-recipes_name = "recipes.txt"
-default_urls_name = "urls.txt"
+known_urls_name:Final[str] = "knownURLs.txt"
+recipes_name:Final[str] = "recipes.txt"
+default_urls_name:Final[str] = "urls.txt"
 
+
+workdir:str = os.path.expanduser(os.path.join("~", "Rezepte"))
+default_data_directory:str = os.path.join(xdg_data_home(), program_name)
+default_cache_directory:str = os.path.join(xdg_cache_home(), program_name)
 if debug:
     workdir = os.path.join(getcwd(), "testfiles")
     default_data_directory = os.path.join(workdir, "data")
     default_cache_directory = os.path.join(workdir, "cache")
-else:
-    workdir = os.path.expanduser(os.path.join("~", "Rezepte"))
-    default_data_directory = os.path.join(xdg_data_home(), program_name)
-    default_cache_directory = os.path.join(xdg_cache_home(), program_name)
+    
 
-known_urls_file = ensure_existence_file(known_urls_name, default_data_directory)
-url_file = ensure_existence_file(default_urls_name, workdir)
-recipe_file = ensure_existence_file(recipes_name, workdir)
+known_urls_file:Final[str] = ensure_existence_file(known_urls_name, default_data_directory)
+url_file:Final[str] = ensure_existence_file(default_urls_name, workdir)
+recipe_file:Final[str] = ensure_existence_file(recipes_name, workdir)
 
-NA = "N/A"
-def getInfo(name, func, data):
+Parsed = NewType('Parsed', recipe_scrapers._abstract.AbstractScraper)
+URL = NewType('URL', str)
+NA:Final[str] = "N/A"
+def getInfo(name:str, func:Callable[[Parsed], Any], data):
     try:
         info = str(func(data))
         if not info or info.isspace():
@@ -78,31 +85,31 @@ def getInfo(name, func, data):
             
     return NA
 
-extraction_instructions = [
+extraction_instructions:Final[list[Tuple[str, Callable[[Parsed], Any]]]] = [
     ("title", lambda data:data.title()),
     ("total time", lambda data: data.total_time()),
     ("yields", lambda data: data.yields()),
     ("ingredients", lambda data: "\n".join(data.ingredients())),
     ("instructions", lambda data: "\n\n".join(data.instructions_list()))
 ]
-between_recipes = "\n\n\n\n\n"
-head_sep = "\n\n"
+between_recipes:Final[str] = "\n\n\n\n\n"
+head_sep:Final[str] = "\n\n"
 
-def recipe2disk(url, recipe):
+def recipe2disk(url:URL, recipe:str) -> None:
     with open(recipe_file, 'a') as file:
         file.write(recipe)
 
     with open(known_urls_file, 'a') as file:
         file.write(url)
 
-def parsed2recipe(url, scraped):
+def parsed2recipe(url:URL, parsed:Parsed) -> None:
     info = {}
     for instruction in extraction_instructions:
-        info[instruction[0]] = getInfo(*instruction, scraped)
+        info[instruction[0]] = getInfo(*instruction, parsed)
     
     if info["ingredients"] is NA and info["instructions"] is NA:
         print("\t", "Nothing worthwhile could be extracted. Skipping...")
-        return None
+        return
     else:
         recipe = "\n".join([info["title"],
                         head_sep,
@@ -114,23 +121,23 @@ def parsed2recipe(url, scraped):
                         "from: " + url,
                         between_recipes])
     
-    return recipe2disk(url, recipe)
+    recipe2disk(url, recipe)
 
-def html2recipe(url, content):
+def html2recipe(url:URL, content:str) -> None:
     print("Processing", url)
     try:
-        parsed = scrape_html(html = content, org_url = url)
+        parsed:Parsed = Parsed(scrape_html(html = content, org_url = url))
     except (WebsiteNotImplementedError,
         NoSchemaFoundInWildMode):
         print("\t", "Unknown Website. Extraction not supported. Skipping...")
-        return None
+        return
     except AttributeError:
         print("\t", "Error while parsing website. Skipping...")
-        return None
+        return
     
-    return parsed2recipe(url, parsed)
+    parsed2recipe(url, parsed)
 
-async def urls2recipes(url_queue, timeout):
+async def urls2recipes(url_queue:asyncio.queues.Queue, timeout:aiohttp.client.ClientTimeout) -> None:
     async with aiohttp.ClientSession(timeout=timeout) as session:
         while not url_queue.empty():
             try:
@@ -138,29 +145,29 @@ async def urls2recipes(url_queue, timeout):
                 async with session.get(url) as response:
                     html = await response.text()
                 
-                recipe = html2recipe(url, html)
+                html2recipe(url, html)
                 
             except (aiohttp.client_exceptions.TooManyRedirects, asyncio.TimeoutError):
-                network_timer.end_multi(timer_id)
                 print("Issue with reaching ", url, ", skipping...")
                 
-def removeTracking(url, *identifiers):
+def removeTracking(url:URL, *identifiers:str) -> URL:
     for i in identifiers:
         start_tracking_part = url.find(i)
         if(start_tracking_part > -1):
             tmp = url[:start_tracking_part]
             if validators.url(tmp):
-                url = tmp
+                url = URL(tmp)
     return url
 
-def processURLs(known_urls, urls):
-    processed = set()
-    for url in urls:
-        url = url.replace(os.linesep, '')
-        url.strip()
-        if not url.startswith("http"):
-            url = "http://"+url
-        if validators.url(url):
+def processURLs(known_urls:set[URL], strings:list[str]) -> set[URL]:
+    processed:set[URL] = set()
+    for string in strings:
+        string = string.replace(os.linesep, '')
+        string.strip()
+        if not string.startswith("http"):
+            string = "http://"+string
+        if validators.url(string):
+            url:URL = URL(string)
             url = removeTracking(url, "/ref=", "?")
             if url in known_urls:
                 print("Already scraped:", url)
@@ -170,10 +177,10 @@ def processURLs(known_urls, urls):
             else:
                 processed.add(url)
         else:
-            print("Not an URL:", url)
+            print("Not an URL:", string)
     return processed
 
-def readfiles(*paths):
+def readfiles(*paths:str) -> list[str]:
     lines = []
     for path in paths:
         path = os.path.expanduser(path)
@@ -186,8 +193,8 @@ def readfiles(*paths):
             print("Not a file:", path)
     return lines
 
-async def fetch(urls):
-    q = asyncio.Queue()
+async def fetch(urls:set[URL]) -> None:
+    q:asyncio.queues.Queue = asyncio.Queue()
     for url in urls: await q.put(url)
     timeout = aiohttp.ClientTimeout(total = 10 * len(urls), connect = 1,
                                     sock_connect=None, sock_read = None)
@@ -201,21 +208,21 @@ if __name__ == "__main__":
         with open(recipe_file, 'w') as file:
             file.write("          REZEPTE\n")
 
+    known_urls:set[URL] = set()
     if os.path.isfile(known_urls_file):
         with open(known_urls_file, 'r') as file:
-            known_urls = set(file.readlines())
-    else:
-        known_urls = set()
+            for url in file.readlines(): known_urls.add(URL(url)) 
     
+    unprocessed:list[str] = []
     if 1 == len(sys.argv):
         unprocessed = readfiles(url_file)
     else:
         if args_are_files:
-            unprocessed = readfiles(sys.argv[1:])
+            unprocessed = readfiles(*sys.argv[1:])
         else:
             unprocessed = sys.argv[1:]
     
-    urls = processURLs(known_urls, unprocessed)
+    urls:set[URL] = processURLs(known_urls, unprocessed)
     asyncio.run(fetch(urls))
 
         
