618a35dfcc203bcbe7ed83f5d8ddf3cfd8e6d6ee
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-03-05 14:25:11 +0100

    Use reworked dprint-function for messaging
    The author discovered that this type functionality is normally taken
    care of by a logging framework. It will be replaced by such in one of
    the following commits. The work is done however so the code will be
    saved into the git-history regardless. Maybe to serve as a testimonial
    to the fact that an hour of work can save five minutes of googling.
    
    If a user only wants error messages for example (eg. lower verbosity
    levels) and the error message needs context, that would only appear
    on higher verbosity levels, there now is the possibility of providing
    those error printouts with a context-tuple. If the verbosity level would
    have been too low for the error message to appear, dprint can now print
    it regardless on a case-by-case basis.
    Example: One dprint-call shows the currently processed URL and the other
    shows a specific error that occurs while processing the URL. If only
    error-messaging was allowed the error message would appear without the
    URL that was being processed while this error occured, thus rendering
    the error message pointless. Through context the error message has
    the option to show the relevant information regardless of the verbosity
    levels.

--- rezepte.py
+++ rezepte.py
@@ -17,8 +17,9 @@ args_are_files:Final[bool] = True
 
 #level 0 -> silent
 #level 1 -> errors
-#level 2 -> events
-#level 3 -> setup
+#level 2 -> proceedings
+#level 3 -> notice
+#level 4 -> setup
 
 vlevel:int = -1
 if debug:
@@ -26,22 +27,34 @@ if debug:
 else:
     vlevel = 0
 
-def dprint(level:int, *args:str, sep:str=' ', end:str='\n', file=None, flush:bool=False) -> None:
-    if level >= vlevel:
-        print(*args, sep=sep, end=end, file=file, flush=flush)
-    
+Context = NewType('Context', Tuple[int, str])
+nocontext:Final[Context] = Context((-1, ""))
+def while_context(context:Context) -> Context:
+    tmp = context[1]
+    tmp = "While " + tmp[0].lower() + tmp[1:] + ":"
+    return Context((context[0],  tmp))
 
+def dprint(level:int, *args:str, sep:str=' ', end:str='\n', file=None, flush:bool=False, context:Context=nocontext) -> Context:
+    if level <= vlevel:
+        if context[0] > vlevel:
+            print(context[1], file=file, flush=flush, end=end)
+        print(*args, sep=sep, end=end, file=file, flush=flush)
+    return Context((level, sep.join(args)))
+    
 def ensure_existence_dir(*pathelements:str) -> str:
     path = os.path.join(*pathelements)
     if path.startswith("~"):
         path = os.path.expanduser(path)
     path = os.path.realpath(path)
+    if not os.path.isdir(path):
+        dprint(4, "Creating directory:", path)
     os.makedirs(path, exist_ok=True)
     return path
 
 def ensure_existence_file(filename:str, *pathelements:str) -> str:
     path = os.path.join(ensure_existence_dir(*pathelements), filename)
     if not os.path.isfile(path):
+        dprint(4, "Creating file:", path)
         with open(path, 'w') as file:
             pass
     return path
@@ -50,7 +63,6 @@ known_urls_name:Final[str] = "knownURLs.txt"
 recipes_name:Final[str] = "recipes.txt"
 default_urls_name:Final[str] = "urls.txt"
 
-
 workdir:str = os.path.expanduser(os.path.join("~", "Rezepte"))
 default_data_directory:str = os.path.join(xdg_data_home(), program_name)
 default_cache_directory:str = os.path.join(xdg_cache_home(), program_name)
@@ -58,7 +70,6 @@ if debug:
     workdir = os.path.join(getcwd(), "testfiles")
     default_data_directory = os.path.join(workdir, "data")
     default_cache_directory = os.path.join(workdir, "cache")
-    
 
 known_urls_file:Final[str] = ensure_existence_file(known_urls_name, default_data_directory)
 url_file:Final[str] = ensure_existence_file(default_urls_name, workdir)
@@ -67,19 +78,19 @@ recipe_file:Final[str] = ensure_existence_file(recipes_name, workdir)
 Parsed = NewType('Parsed', recipe_scrapers._abstract.AbstractScraper)
 URL = NewType('URL', str)
 NA:Final[str] = "N/A"
-def getInfo(name:str, func:Callable[[Parsed], Any], data):
+def getInfo(name:str, func:Callable[[Parsed], Any], data, context:Context):
     try:
         info = str(func(data))
         if not info or info.isspace():
-            print("\t", name.capitalize(), "contains nothing")
+            dprint(1, "\t", name.capitalize(), "contains nothing", context=context)
             info = NA
         return info
     except (SchemaOrgException, ElementNotFoundInHtml, TypeError, AttributeError):
-        print("\t", "No", name, "found")
+        dprint(1, "\t", "No", name, "found", context=context)
     except NotImplementedError:
-        print("\t", name.capitalize(), "not implemented for this website")
+        dprint(1, "\t", name.capitalize(), "not implemented for this website", context=context)
     except Exception as e:
-        print("\t", "Error while extracting", name)
+        dprint(1, "\t", "Error while extracting", name, context=context)
         if(debug):
             traceback.print_exception(e)
             
@@ -102,13 +113,14 @@ def recipe2disk(url:URL, recipe:str) -> None:
     with open(known_urls_file, 'a') as file:
         file.write(url)
 
-def parsed2recipe(url:URL, parsed:Parsed) -> None:
+def parsed2recipe(url:URL, parsed:Parsed, context:Context) -> None:
     info = {}
     for instruction in extraction_instructions:
-        info[instruction[0]] = getInfo(*instruction, parsed)
+        info[instruction[0]] = getInfo(*instruction, parsed, context=context)
+        if info[instruction[0]] is NA: context = nocontext
     
     if info["ingredients"] is NA and info["instructions"] is NA:
-        print("\t", "Nothing worthwhile could be extracted. Skipping...")
+        dprint(1, "\t", "Nothing worthwhile could be extracted. Skipping...", context=context)
         return
     else:
         recipe = "\n".join([info["title"],
@@ -124,31 +136,34 @@ def parsed2recipe(url:URL, parsed:Parsed) -> None:
     recipe2disk(url, recipe)
 
 def html2recipe(url:URL, content:str) -> None:
-    print("Processing", url)
+    context:Context = dprint(4, "Processing", url)
+    context = while_context(context)
     try:
         parsed:Parsed = Parsed(scrape_html(html = content, org_url = url))
     except (WebsiteNotImplementedError,
         NoSchemaFoundInWildMode):
-        print("\t", "Unknown Website. Extraction not supported. Skipping...")
+        dprint(1, "\t", "Unknown Website. Extraction not supported. Skipping...", context = context)
         return
     except AttributeError:
-        print("\t", "Error while parsing website. Skipping...")
+        dprint(1, "\t", "Error while parsing website. Skipping...", context = context)
         return
     
-    parsed2recipe(url, parsed)
+    parsed2recipe(url, parsed, context)
 
 async def urls2recipes(url_queue:asyncio.queues.Queue, timeout:aiohttp.client.ClientTimeout) -> None:
     async with aiohttp.ClientSession(timeout=timeout) as session:
         while not url_queue.empty():
             try:
                 url = await url_queue.get()
+                context:Context = dprint(4, "Fetching", url)
+                context = while_context(context)
                 async with session.get(url) as response:
                     html = await response.text()
                 
                 html2recipe(url, html)
                 
             except (aiohttp.client_exceptions.TooManyRedirects, asyncio.TimeoutError):
-                print("Issue with reaching ", url, ", skipping...")
+                dprint(1, "\t", "Issue reaching website, skipping...", context=context)
                 
 def removeTracking(url:URL, *identifiers:str) -> URL:
     for i in identifiers:
@@ -170,14 +185,14 @@ def processURLs(known_urls:set[URL], strings:list[str]) -> set[URL]:
             url:URL = URL(string)
             url = removeTracking(url, "/ref=", "?")
             if url in known_urls:
-                print("Already scraped:", url)
+                dprint(3, "Already scraped:", url)
                 continue
             if url in processed:
-                print("Already queued:", url)
+                dprint(3, "Already queued:", url)
             else:
                 processed.add(url)
         else:
-            print("Not an URL:", string)
+            dprint(3, "Not an URL:", string)
     return processed
 
 def readfiles(*paths:str) -> list[str]:
@@ -186,11 +201,12 @@ def readfiles(*paths:str) -> list[str]:
         path = os.path.expanduser(path)
         path = os.path.realpath(path)
         if os.path.isfile(url_file):
+            dprint(4, "Reading", path)
             with open(url_file, 'r') as file:
                 for line in file.readlines():
                     lines.append(line)
         else:
-            print("Not a file:", path)
+            dprint(1, "Not a file:", path)
     return lines
 
 async def fetch(urls:set[URL]) -> None:
