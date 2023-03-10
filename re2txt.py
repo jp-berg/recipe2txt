bc6164765ce13616c3c390dab748ec5f8be1b4b3
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-03-10 13:18:37 +0100

    Rename rezepte.py to main.py

--- re2txt.py
+++ re2txt.py
@@ -1,6 +1,6 @@
-import recipe2txt.rezepte
+import recipe2txt.main
 from sys import argv
 
 if __name__ == '__main__':
-    recipe2txt.rezepte.main(argv[1:], debug=True, args_are_files=True, verbosity=4)
+    recipe2txt.main.main(argv[1:], debug=True, args_are_files=True, verbosity=4)
 
