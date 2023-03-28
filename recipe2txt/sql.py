e8cdc0182e70b4347caa8764dcf1b496d76169c9
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-03-28 16:15:18 +0200

    Finalize SQL-interface-class
    Cached recipe data will be stored in an sqlite-database. Fetching the
    same recipe again will result in a database-read instead of a http-
    request, potentially saving time and resources. Incomplete information
    will automatically be added, if for example a new version of
    recipe-scrapers is used.

--- recipe2txt/sql.py
+++ recipe2txt/sql.py
@@ -1,21 +1,23 @@
 import sqlite3
-from typing import Final
+from typing import Final, Tuple, Optional, TypeGuard, NewType
 from .utils.misc import *
+from .html2recipe import Recipe, NA, recipe_attributes, SCRAPER_VERSION, gen_status, RecipeStatus as RS
 
 _CREATE_TABLES: Final[str] = """
 CREATE TABLE IF NOT EXISTS recipes(
-	recipeID      INTEGER NOT NULL,
-	url	      TEXT NOT NULL UNIQUE,
-	status	      TEXT NOT NULL,
-	last_fetched  TEXT DEFAULT (datetime()),
-	host	      TEXT,
-	title	      TEXT,
-	total_time    INTEGER,
-	image	      TEXT,
-	ingredients   TEXT,
-	instructions  TEXT,
-	yields	      TEXT,
-	nutrients     TEXT,
+	recipeID        INTEGER NOT NULL,
+	url             TEXT NOT NULL UNIQUE,
+	status	        INTEGER NOT NULL,
+	last_fetched    TEXT DEFAULT (datetime()),
+	scraper_version TEXT,
+	host	        TEXT,
+	title	        TEXT,
+	total_time      TEXT,
+	image	        TEXT,
+	ingredients     TEXT,
+	instructions    TEXT,
+	yields	        TEXT,
+	nutrients       TEXT,
 	PRIMARY KEY(recipeID AUTOINCREMENT)
 ) STRICT;
 CREATE TABLE IF NOT EXISTS files(
@@ -27,27 +29,171 @@ CREATE TABLE IF NOT EXISTS files(
 CREATE TABLE IF NOT EXISTS contents(
 	fileID	 INTEGER NOT NULL,
 	recipeID INTEGER NOT NULL,
-	FOREIGN KEY(fileID) REFERENCES files(fileID) ON DELETE CASCADE,
-	FOREIGN KEY(recipeID) REFERENCES recipes(recipeID) ON DELETE CASCADE
+	FOREIGN KEY(fileID) REFERENCES files(fileID) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY(recipeID) REFERENCES recipes(recipeID) ON UPDATE CASCADE ON DELETE CASCADE
 ) STRICT;
 """
 
-_INSERT_RECIPE: Final[str] = "INSERT INTO recipes" \
-                             " ( url, status, host, title, total_time, image, ingredients, instructions, yields )" \
-                             " VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ? )"
+recipe_row_attributes: Final[list[str]] = recipe_attributes + [
+    "recipeID",
+    "last_fetched"
+]
 
-_INSERT_FILE: Final[str] = "INSERT INTO files ( filepath ) VALUES ( ? )"
+_INSERT_RECIPE: Final[str] = "INSERT OR IGNORE INTO recipes" + \
+                             " (" + ", ".join(recipe_attributes) + ")" + \
+                             " VALUES (" + ("?," * len(recipe_attributes))[:-1] + ")"
 
-_ASSOCIATE_FILE_RECIPE: Final[str] = "INSERT INTO contents (fileID, recipeID) VALUES (" \
-                                     "(SELECT fileID FROM files WHERE filepath = ?)" \
-                                     "(SELECT recipeID FROM recipes WHERE url = ?))"
+_INSERT_OR_REPLACE_RECIPE: Final[str] = "INSERT OR REPLACE INTO recipes" + \
+                                        " (" + ", ".join(recipe_attributes) + ")" + \
+                                        " VALUES (" + ("?," * len(recipe_attributes))[:-1] + ")"
 
-attibutes = ["host", "title", "total_time", "image", "ingredients", "instructions", "yields", "nutrients"]
+_INSERT_FILE: Final[str] = "INSERT OR IGNORE INTO files ( filepath ) VALUES ( ? )"
 
-class data:
-    def __init__(self, data_path: File) -> None:
-        self.con = sqlite3.connect(data_path)
+_ASSOCIATE_FILE_RECIPE: Final[str] = "INSERT OR IGNORE INTO contents (fileID, recipeID) VALUES (" \
+                                     " (SELECT fileID FROM files WHERE filepath = ?)," \
+                                     " (SELECT recipeID FROM recipes WHERE url = ?))"
+
+_GET_RECIPE: Final[str] = "SELECT " + ", ".join(recipe_attributes) + " FROM recipes WHERE url = ?"
+_GET_RECIPES: Final[str] = "SELECT " + ", ".join(recipe_attributes) + " FROM ((SELECT * FROM files WHERE filepath = ?)" \
+                           " NATURAL JOIN contents NATURAL JOIN recipes) WHERE status >= " + \
+                           str(int(RS.INCOMPLETE_ON_DISPLAY))
+_GET_URLS_STATUS_VERSION: Final[str] = "SELECT url, status, scraper_version FROM recipes"
+_GET_CONTENT: Final[str] = "SELECT url FROM ((SELECT * FROM files WHERE filepath = ?)" \
+                           " NATURAL JOIN contents NATURAL JOIN recipes)"
+
+
+AccessibleDatabase = NewType("AccessibleDatabase", str)
+
+
+def is_accessible_db(path: str) -> TypeGuard[AccessibleDatabase]:
+    try:
+        con = sqlite3.connect(path)
+    except sqlite3.OperationalError:
+        return False
+    cur = con.cursor()
+
+    try:
+        cur.execute("PRAGMA SCHEMA_VERSION")
+    except sqlite3.DatabaseError:
+        return False
+    finally:
+        cur.close()
+        con.close()
+    return True
+
+
+def fetch_again(status: RS, scraper_version: str) -> bool:
+    if status == RS.UNREACHABLE or status == RS.INCOMPLETE_ESSENTIAL:
+        return True
+
+    if (status == RS.UNKNOWN
+        or status == RS.INCOMPLETE_ON_DISPLAY
+        or status == RS.COMPLETE_ON_DISPLAY) \
+            and scraper_version < SCRAPER_VERSION:
+        return True
+
+    return False
+
+
+class Database:
+    def __init__(self, database: AccessibleDatabase, output_file: File) -> None:
+        self.con = sqlite3.connect(database)
         self.cur = self.con.cursor()
-        self.cur.execute(_CREATE_TABLES)
+        self.cur.executescript(_CREATE_TABLES)
+        self.filepath = output_file
+        self.cur.execute(_INSERT_FILE, (output_file,))
 
-    def newRecipe(self, url:URL, html):
+    def new_recipe(self, recipe: Recipe) -> Recipe:
+        self.cur.execute(_INSERT_RECIPE, tuple(recipe))
+        self.cur.execute(_ASSOCIATE_FILE_RECIPE, (self.filepath, recipe.url))
+        self.con.commit()
+        return recipe
+
+    def replace_recipe(self, recipe: Recipe) -> Recipe:
+        self.cur.execute(_INSERT_OR_REPLACE_RECIPE, tuple(recipe))
+        self.cur.execute(_ASSOCIATE_FILE_RECIPE, (self.filepath, recipe.url))
+        self.con.commit()
+        return recipe
+
+    def get_recipe_row(self, url: URL) -> Optional[Tuple]:
+        self.cur.execute(_GET_RECIPE, (url,))
+        r = self.cur.fetchone()
+        return r
+
+    def get_recipe(self, url: URL) -> Optional[Recipe]:
+        row = self.get_recipe_row(url)
+        r = Recipe(*row) # type: ignore
+        return r
+
+    def get_recipes(self) -> list[Recipe]:
+        self.cur.execute(_GET_RECIPES, (self.filepath,))
+        recipes = [Recipe(*row) for row in self.cur.fetchall()]
+        return recipes
+
+    def urls_to_fetch(self, wanted: set[URL]) -> set[URL]:
+        self.cur.execute(_GET_URLS_STATUS_VERSION)
+        available = self.cur.fetchall()
+        for url, status, version in available:
+            if url in wanted and not fetch_again(status, version):
+                dprint(3, "Using cached version", url)
+                if status <= RS.UNKNOWN: print(status)
+                wanted.remove(url)
+                if not wanted: break
+        return wanted
+
+    def insert_recipe_unreachable(self, url: URL) -> Recipe:
+        r = Recipe(url=url, status=RS.UNREACHABLE, scraper_version=SCRAPER_VERSION)
+        return self.insert_recipe(r)
+
+    def insert_recipe_unknown(self, url: URL) -> Recipe:
+        r = Recipe(url=url, status=RS.UNKNOWN, scraper_version=SCRAPER_VERSION)
+        return self.insert_recipe(r)
+
+    def insert_recipe(self, recipe: Recipe, prefer_new: bool = False) -> Recipe:
+        old_row = self.get_recipe_row(recipe.url)
+        new_row = tuple(recipe)
+        merged_row = []
+        updated = []
+        if old_row:
+            for old_val, new_val in zip(old_row, new_row):
+                if new_val and new_val != NA:
+                    if old_val and old_val != NA:
+                        if prefer_new:
+                            merged_row.append(new_val)
+                            updated.append(True)
+                        else:
+                            merged_row.append(old_val)
+                            updated.append(False)
+                    else:
+                        merged_row.append(new_val)
+                        updated.append(True)
+                else:
+                    merged_row.append(old_val)
+                    updated.append(False)
+
+            merged_row[-1] = SCRAPER_VERSION
+            if True in updated:
+                merged_row[-2] = gen_status(merged_row)  # type: ignore
+                r = Recipe(*merged_row)  # type: ignore
+                replaced_list = ["\t{}: {} => {}".format(attr, head_str(old_val), head_str(new_val))
+                            for attr, old_val, new_val, is_replaced in zip(recipe_attributes, old_row, new_row, updated)
+                            if is_replaced]
+                replaced = "\n" + "\n".join(replaced_list)
+                dprint(3, "Updated " + recipe.url + "", replaced)
+                self.replace_recipe(r)
+            else:
+                r = Recipe(*merged_row)  # type: ignore
+            return r
+        else:
+            self.new_recipe(recipe)
+            return recipe
+
+    def get_contents(self) -> list[URL]:
+        self.cur.execute(_GET_CONTENT, (self.filepath,))
+        urls = [URL(url[0]) for url in self.cur.fetchall()]
+        return urls
+
+    def __del__(self):
+        self.cur.close()
+        self.con.close()
+
