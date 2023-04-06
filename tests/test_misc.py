eaf611c8801ab82fd4b819db4ceae3ad36defc15
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-04-06 20:22:42 +0200

    Add (some) testing for utils/misc.py

---
+++ tests/test_misc.py
@@ -0,0 +1,91 @@
+import unittest
+import recipe2txt.utils.misc as misc
+import os
+
+file_dir = os.path.dirname(__file__)
+work_dir = os.getcwd()
+misc.set_vlevel(0)
+
+testdirs = ["TESTFOLDER1", "TESTFOLDER2"]
+testfile = "TESTFILE.txt"
+none_dirs = [["/dev", "null"] + testdirs]
+normal_dirs = [[work_dir] + testdirs, [file_dir] + testdirs]
+
+
+class FileTests(unittest.TestCase):
+
+    def test_full_path(self):
+        params = [
+            (["~", "Documents", "File1"], os.path.expanduser(os.path.join("~", "Documents", "File1"))),
+            (["  /tmp", "dir1", "file2.txt  "], os.path.join("/tmp", "dir1", "file2.txt"))]
+
+        for test, validation in params:
+            with self.subTest(i=test):
+                self.assertEqual(misc.full_path(*test), validation)
+
+    def test_ensure_existence_dir(self):
+        params_path = [(test, os.path.join(*test)) for test in normal_dirs]
+
+        for test, validation in params_path:
+            with self.subTest(i=test):
+                self.assertEqual(misc.ensure_existence_dir(*test), validation)
+                os.removedirs(validation)
+
+        for test in none_dirs:
+            with self.subTest(i=test):
+                self.assertIsNone(misc.ensure_existence_dir(*test))
+
+    def test_ensure_accessible_file(self):
+        params_path = [(test, os.path.join(*test, testfile)) for test in normal_dirs]
+
+        for test, validation in params_path:
+            with self.subTest(i=test):
+                self.assertEqual(misc.ensure_accessible_file(testfile, *test), validation)
+                if not os.path.isfile(validation):
+                    self.fail("File", validation, "was not created")
+                try:
+                    with open(validation, "w") as file:
+                        file.write("TEST")
+                    with open(validation, "r") as file:
+                        content = file.readline().rstrip(os.linesep)
+                        self.assertEqual(content, "TEST")
+                except OSError as e:
+                    self.fail(e)
+
+                os.remove(validation)
+                os.removedirs(os.path.dirname(validation))
+
+        for test in none_dirs:
+            self.assertIsNone(misc.ensure_accessible_file(testfile, *test))
+
+
+class StrTests(unittest.TestCase):
+
+    def test_cutoff(self):
+        urls = [("https://www.shop.com/product?utm_source=searchpage", "https://www.shop.com/product"),
+                ("https://www.info.net/important-message?user-id:12345", "https://www.info.net/important-message"),
+                ("http://www.blog.org/entry1/ref=referer", "http://www.blog.org/entry1")]
+
+        for url, validation in urls:
+            with self.subTest(i=url):
+                self.assertEqual(misc.cutoff(url, "?", "/ref="), validation)
+
+    def test_dict2str(self):
+        dicts = [({1: "one", 2: "two", 3: "three"}, os.linesep.join(["1: one", "2: two", "3: three"])),
+                 ({"one": "Eins", "two": "Zwei", "three": "Drei"},
+                  os.linesep.join(["one: Eins", "two: Zwei", "three: Drei"]))]
+
+        for d, validation in dicts:
+            with self.subTest(i=d):
+                self.assertEqual(misc.dict2str(d), validation)
+
+    def test_head_str(self):
+        objects = [("teststringteststringteststring", "teststr..."),
+                   ("teststring", "teststring"),
+                   ("test       ", "test..."),
+                   ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "[1, 2,...")]
+
+        for obj, validation in objects:
+            with self.subTest(i=obj):
+                self.assertEqual(misc.head_str(obj, 10), validation)
+
