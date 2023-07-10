a70ecffadf56ebbb0a1fabbb822d5823a654020a
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-07-10 16:50:54 +0200

    Make QueueContextManager asyncio-compatible
    The stream-logger is now able to handle the specific contexts for
    multiple tasks simultaneously. Log-messages can be deferred until the
    end of the context to avoid incoherent logs by messages from different
    tasks getting jumbled together.

--- recipe2txt/utils/ContextLogger.py
+++ recipe2txt/utils/ContextLogger.py
@@ -1,9 +1,10 @@
 import contextlib
+import asyncio
 import logging
 from os import linesep
 from logging.handlers import RotatingFileHandler
 from types import TracebackType
-from typing import Final, Callable, Literal, Any, Generator
+from typing import Final, Callable, Literal, Any, Generator, Optional
 from sys import version_info
 if version_info >= (3, 11):
     from typing import LiteralString
@@ -25,81 +26,143 @@ _LOG_FORMAT_FILE: Final[LiteralString] = "%(asctime)s - %(levelname)s %(module)s
 DATEFMT: Final[LiteralString] = "%Y-%m-%d %H:%m:%S"
 
 CTX_ATTR: Final[LiteralString] = "is_context"
+DEFER_EMIT: Final[LiteralString] = "defer_emit"
 IS_CONTEXT: Final[dict[str, bool]] = {CTX_ATTR: True}
 END_CONTEXT: Final[dict[str, bool]] = {CTX_ATTR: False}
-WHILE: Final[str] = f"While %s:{linesep}"
+WHILE: Final[str] = f"While %s:{linesep}\t"
 DO_NOT_LOG: Final[LiteralString] = "THIS MESSAGE SHOULD NOT BE LOGGED"
+DEFAULT_CONTEXT: Final[LiteralString] = "default_context"
 
 logger_list: list[logging.Logger] = []
+_stream_handler: Optional[logging.StreamHandler[Any]] = None
 
 
-## ASSUMTIONS:  no threading/async while context,
-##              level does not change in context,
+class Context:
+    def __init__(self) -> None:
+        self.context_msg: str = ""
+        self.context_args: Any = ()
+        self.with_context: bool = False
+        self.triggered: bool = False
+        self.context_id: int = 0
+        self.defer_emit: bool = False
+        self.deferred_records: list[logging.LogRecord] = []
+
+    def reset(self) -> None:
+        self.context_msg = ""
+        self.context_args = ()
+        self.with_context = False
+        self.triggered = False
+        self.context_id = 0
+        self.defer_emit = False
+        self.deferred_records.clear()
+
+    def dispatch(self, record: logging.LogRecord) -> bool:
+        if self.defer_emit:
+            self.deferred_records.append(record)
+            return False
+        else:
+            return True
+
+    def set_context(self, record: logging.LogRecord, log_level: int) -> bool:
+        self.defer_emit = bool(getattr(record, DEFER_EMIT, False))
+        self.with_context = True
+        if log_level <= record.levelno:
+            self.triggered = True
+            return self.dispatch(record)
+        else:
+            self.with_context = True
+            self.context_msg = record.msg
+            self.context_args = record.args
+            self.triggered = False
+            return False
+
+    def close_context(self) -> None:
+        if self.with_context and self.deferred_records:
+            if _stream_handler:
+                for record in self.deferred_records:
+                    _stream_handler.emit(record)
+            else:
+                logging.warning("No stream handler configured.")
+        self.reset()
+
+    def get_context(self) -> str:
+        if not self.triggered:
+            self.context_msg = str(self.context_msg)
+            context = WHILE % (self.context_msg[0].lower() + self.context_msg[1:])
+            self.triggered = True
+            return str(context % self.context_args)
+        else:
+            return "\t"
+
+    def process(self, record: logging.LogRecord, log_level: int) -> bool:
+        record.ctx = ""
+
+        is_context = getattr(record, CTX_ATTR, None)
+        if is_context:
+            return self.set_context(record, log_level)
+        elif is_context is False:
+            self.close_context()
+
+        if record.msg == DO_NOT_LOG:
+            return False
+
+        if log_level <= record.levelno:  # If record should be emitted
+            if self.with_context:
+                record.ctx = self.get_context()
+            return self.dispatch(record)
+
+        return False
+
+
+## ASSUMTIONS:  level does not change in context,
 ##              logger is the only output to the command line
-##              only one context at a time
-##              Expensive __str__() is guarded by .isEnabledFor()
+##              No threading while logging
 
 
 class QueueContextFilter(logging.Filter):
     def __init__(self, log_level: int) -> None:
         self.log_level = log_level
-        self.context_msg = ""
-        self.context_args = ()
-        self.with_context = False
-        self.triggered = False
+        # TODO: Add reset
+        self.tasklocal_context: dict[str, Context] = {DEFAULT_CONTEXT: Context()}
+
+    def get_context(self) -> Context:
+        try:
+            t = asyncio.current_task()
+        except RuntimeError:
+            t = None
+        task_name = t.get_name() if t else DEFAULT_CONTEXT
+        context = self.tasklocal_context.get(task_name)
+        if not context:
+            context = Context()
+            self.tasklocal_context[task_name] = context
+        return context
 
     def set_level(self, log_level: int) -> int:
-        if self.with_context:
+        if self.get_context().with_context:
             logging.error("Modifying logging-level during context is not possible")
         else:
             self.log_level = log_level
         return self.log_level
 
     def filter(self, record: logging.LogRecord) -> bool:
-        is_context = getattr(record, CTX_ATTR, None)
-        if is_context is False:
-            self.context_msg = ""
-            self.context_args = ()
-            self.with_context = False
-            self.triggered = False
-
-        if record.msg == DO_NOT_LOG:
-            return False
-
-        if self.log_level <= record.levelno:  # If record should be emitted
-            if not is_context and self.with_context:
-                record.ctx = "\t"
-                if not self.triggered:
-                    self.context_msg = str(self.context_msg)
-                    context = WHILE % (self.context_msg[0].lower() + self.context_msg[1:])
-                    record.msg = (context % self.context_args) + str(record.msg)
-                    self.triggered = True
-            else:
-                if is_context:
-                    self.with_context = True
-                    self.triggered = True
-                record.ctx = ""
-            return True
-
-        if is_context:
-            self.with_context = True
-            self.context_msg = record.msg
-            self.context_args = record.args  # type:ignore
-            self.triggered = False
-        return False
+        var = self.get_context()
+        return var.process(record, self.log_level)
 
 
 class QueueContextManager:
 
-    def __init__(self, logger: logging.Logger, logging_fun: Callable[..., None], msg: str, *args: Any, **kwargs: Any):
+    def __init__(self, logger: logging.Logger, logging_fun: Callable[..., None], msg: str, *args: Any,
+                 defer_emit: bool = False, **kwargs: Any):
         self.args = args
         self.kwargs = kwargs
         self.logging_fun = logging_fun
         self.msg = msg
         self.logger = logger
+        self.defer_emit = defer_emit
 
-    def __enter__(self) -> None:  # TODO: Make enter defer emit until exit
-        self.logging_fun(self.msg, *self.args, stacklevel=2, extra=IS_CONTEXT, **self.kwargs)
+    def __enter__(self) -> None:
+        extra = {CTX_ATTR: True, DEFER_EMIT: self.defer_emit}
+        self.logging_fun(self.msg, *self.args, stacklevel=2, extra=extra, **self.kwargs)
 
     def __exit__(self, exc_type: type, exc_value: BaseException, traceback: TracebackType) -> Literal[False]:
         if not (exc_type or exc_value or traceback):
@@ -151,6 +214,10 @@ def get_stream_handler(level: int = logging.WARNING) -> logging.StreamHandler[An
     f = QueueContextFilter(level)
     stream_handler.addFilter(f)
     stream_handler.setFormatter(logging.Formatter(_LOG_FORMAT_STREAM))
+
+    global _stream_handler
+    _stream_handler = stream_handler
+
     return stream_handler
 
 
