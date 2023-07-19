efa0a0d5243dc6f4029dec58ec58e927ddd3d2d0
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-07-19 13:19:05 +0200

    Add Formatter
    This avoids applying formatting to a record directly (and thus
    possibly disrupting the work of other handlers on the record). This
    also delays formatting until the last possible moment.

--- recipe2txt/utils/ContextLogger.py
+++ recipe2txt/utils/ContextLogger.py
@@ -10,7 +10,6 @@ from typing import Final, Callable, Literal, Any, Generator, Optional
 from recipe2txt.utils.conditional_imports import LiteralString
 from recipe2txt.utils.traceback_utils import shorten_paths
 
-
 string2level: Final[dict[LiteralString, int]] = {
     "debug": logging.DEBUG,
     "info": logging.INFO,
@@ -27,31 +26,19 @@ DATEFMT: Final[LiteralString] = "%Y-%m-%d %H:%m:%S"
 
 CTX_ATTR: Final[LiteralString] = "is_context"
 DEFER_EMIT: Final[LiteralString] = "defer_emit"
-IS_CONTEXT: Final[dict[str, bool]] = {CTX_ATTR: True}
+CTX_MSG_ATTR: Final[LiteralString] = "context_msg"
+CTX_ARGS_ATTR: Final[LiteralString] = "context_args"
+WITH_CTX_ATTR: Final[LiteralString] = "with_context"
+FULL_TRACE_ATTR: Final[LiteralString] = "full_trace"
+
 END_CONTEXT: Final[dict[str, bool]] = {CTX_ATTR: False}
+
 WHILE: Final[str] = f"While %s:{linesep}\t"
 DO_NOT_LOG: Final[LiteralString] = "THIS MESSAGE SHOULD NOT BE LOGGED"
 DEFAULT_CONTEXT: Final[LiteralString] = "default_context"
 
 logger_list: list[logging.Logger] = []
 _stream_handler: Optional[logging.StreamHandler[Any]] = None
-
-
-def format_exception(exc_info: tuple[type[BaseException], BaseException, TracebackType | None],
-                     indent_for_context: bool = False, full: bool = False) -> str:
-    ex_class, exception, trace = exc_info
-    if full:
-        tb_ex = traceback.TracebackException.from_exception(exception)
-        tb_ex.stack = shorten_paths(tb_ex.stack, skip_first=True)
-        tb = tb_ex.format()
-        indent = "\t\t" if indent_for_context else "\t"
-        tb_lines = [indent + line + os.linesep
-                    for frame in tb for line in frame.split(os.linesep) if line]
-        formatted = linesep + "".join(tb_lines)
-    else:
-        formatted = f"{ex_class.__name__} - {exception}"
-
-    return formatted
 
 
 class Context:
@@ -73,17 +60,9 @@ class Context:
         self.defer_emit = False
         self.deferred_records.clear()
 
-    def attach_exception(self, record: logging.LogRecord, log_level: int) -> logging.LogRecord:
-        exc_info = record.exc_info
-        if exc_info and exc_info[0] and exc_info[1]:
-            record.exc_info = None
-            record.exc_text = None
-            formatted_exception = format_exception(exc_info, self.with_context, log_level <= logging.DEBUG)
-            record.msg = record.msg + formatted_exception
-        return record
-
     def dispatch(self, record: logging.LogRecord, log_level: int) -> bool:
-        self.attach_exception(record, log_level)
+        if record.exc_info:
+            setattr(record, FULL_TRACE_ATTR, log_level <= logging.DEBUG)
         if self.defer_emit:
             self.deferred_records.append(record)
             return False
@@ -112,17 +91,7 @@ class Context:
                 logging.warning("No stream handler configured.")
         self.reset()
 
-    def get_context(self) -> str:
-        if not self.triggered:
-            self.context_msg = str(self.context_msg)
-            context = WHILE % (self.context_msg[0].lower() + self.context_msg[1:])
-            self.triggered = True
-            return str(context % self.context_args)
-        else:
-            return "\t"
-
     def process(self, record: logging.LogRecord, log_level: int) -> bool:
-        record.ctx = ""
         is_context = getattr(record, CTX_ATTR, None)
         if is_context:
             return self.set_context(record, log_level)
@@ -134,7 +103,10 @@ class Context:
 
         if log_level <= record.levelno:  # If record should be emitted
             if self.with_context:
-                record.ctx = self.get_context()
+                if not self.triggered:
+                    setattr(record, CTX_MSG_ATTR, self.context_msg)
+                    setattr(record, CTX_ARGS_ATTR, self.context_args)
+                setattr(record, WITH_CTX_ATTR, True)
             return self.dispatch(record, log_level)
 
         return False
@@ -173,6 +145,64 @@ class QueueContextFilter(logging.Filter):
     def filter(self, record: logging.LogRecord) -> bool:
         var = self.get_context()
         return var.process(record, self.log_level)
+
+
+def format_exception(exc_info: tuple[type[BaseException], BaseException, TracebackType | None],
+                     indent_for_context: bool = False, full: bool = False) -> str:
+    ex_class, exception, trace = exc_info
+    if full:
+        tb_ex = traceback.TracebackException.from_exception(exception)
+        tb_ex.stack = shorten_paths(tb_ex.stack, skip_first=True)
+        tb = tb_ex.format()
+        indent = "\t\t" if indent_for_context else "\t"
+        tb_lines = [indent + line + os.linesep
+                    for frame in tb for line in frame.split(os.linesep) if line]
+        formatted = linesep + "".join(tb_lines)
+    else:
+        formatted = f"{ex_class.__name__} - {exception}"
+
+    return formatted
+
+
+def format_context(msg: Any, args: Any) -> str:
+    msg = str(msg)
+    msg = msg[0].lower() + msg[1:]
+    msg = WHILE % msg
+    return str(msg % args)
+
+
+def set_context(record: logging.LogRecord) -> logging.LogRecord:
+    with_context = getattr(record, WITH_CTX_ATTR, False)
+    if with_context:
+        context_msg = getattr(record, CTX_MSG_ATTR, None)
+        if context_msg:
+            fmt_ctx = format_context(context_msg, getattr(record, CTX_ARGS_ATTR, None))
+            record.ctx = fmt_ctx
+        else:
+            record.ctx = "\t"
+    else:
+        record.ctx = ""
+    return record
+
+
+class QueueContextFormatter(logging.Formatter):
+
+    def format(self, record: logging.LogRecord) -> str:
+        record = set_context(record)
+        exc_info = None
+        exc_text = None
+        fmt_ex = ""
+        if record.exc_info and record.exc_info[0] and record.exc_info[1]:
+            exc_info, record.exc_info = record.exc_info, exc_info
+            exc_text, record.exc_text = record.exc_text, exc_text
+            fmt_ex = format_exception(exc_info, indent_for_context=bool(getattr(record, "ctx", "")),
+                                      full=getattr(record, FULL_TRACE_ATTR, False))
+        s = super().format(record) + fmt_ex
+
+        record.exc_info = exc_info
+        record.exc_text = exc_text
+
+        return s
 
 
 class QueueContextManager:
@@ -239,7 +269,7 @@ def get_stream_handler(level: int = logging.WARNING) -> logging.StreamHandler[An
     stream_handler.setLevel(logging.DEBUG)
     f = QueueContextFilter(level)
     stream_handler.addFilter(f)
-    stream_handler.setFormatter(logging.Formatter(_LOG_FORMAT_STREAM))
+    stream_handler.setFormatter(QueueContextFormatter(_LOG_FORMAT_STREAM))
 
     global _stream_handler
     _stream_handler = stream_handler
