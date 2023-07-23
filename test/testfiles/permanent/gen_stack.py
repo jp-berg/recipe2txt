6e5f56a91df18fc4f158ac7a43e084141e22707a
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-07-23 18:28:01 +0200

    Add typing to gen_stack-module

--- test/testfiles/permanent/gen_stack.py
+++ test/testfiles/permanent/gen_stack.py
@@ -1,3 +1,4 @@
+import logging
 from math import sqrt
 from pathlib import Path
 import traceback
@@ -5,50 +6,54 @@ import traceback
 from recipe2txt.utils.traceback_utils import get_shared_frames, format_stacks
 from recipe2txt.utils.misc import ensure_accessible_file
 
+logger = logging.getLogger(__name__)
 
-def fun0_1(x):
+
+def fun0_1(x: float | int) -> float:
     return sqrt(x)
 
 
-def fun0_2(x):
+def fun0_2(x: float | int) -> float:
     return 13 / x
 
 
-def fun1_2(x):
+def fun1_2(x: float | int) -> float:
     return fun0_2(x) * 10
 
 
-def fun1_1(x):
+def fun1_1(x: float | int)  -> float:
     return fun0_1(x) * 3
 
 
-def fun2(x):
+def fun2(x: float | int) -> float:
     if x > 12:
+        logger.debug("Calling fun1_2") # Check logging indentation from different function
         return fun1_2(x * 0)
     else:
+        logger.debug("Calling fun1_1")
         return fun1_1(x) - 12
 
 
-def fun3(x):
+def fun3(x: float | int) -> float:
     return fun2(x) * 4.56
 
 
-def fun4(x):
+def fun4(x: float | int) -> float:
     return fun3(x) + 0.1
 
 
-def fun5(x):
+def fun5(x: float | int) -> float:
     return fun4(x) + 4321
 
 
 class GenTraces:
-    def __init__(self, *values):
-        self.tb_ex_list = []
-        self.error_vals = []
+    def __init__(self, *values: int) -> None:
+        self.tb_ex_list: list[traceback.TracebackException] = []
+        self.error_vals: list[int] = []
         self.res = [self._provoke_exception(value) for value in values]
         self.values = values
 
-    def _provoke_exception(self, x):
+    def _provoke_exception(self, x: int) -> float | None:
         try:
             y = fun5(x)
         except Exception as e:
@@ -57,7 +62,7 @@ class GenTraces:
             return None
         return y
 
-    def get_formatted(self):
+    def get_formatted(self) -> list[str]:
         name = "_".join([str(val) for val in self.error_vals]) + ".txt"
         if not (file := ensure_accessible_file(Path(__file__).parent, "traces", name)):
             raise OSError(f"File could not be created: {name}")
