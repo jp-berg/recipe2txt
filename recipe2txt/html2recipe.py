267263336a9e2e345b2ae1307c5a3e8cd51734b6
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-07-24 17:54:08 +0200

    Add licensing

--- recipe2txt/html2recipe.py
+++ recipe2txt/html2recipe.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2023 Jan Philipp Berg <git.7ksst@aleeas.com>
+#
+# This file is part of recipe2txt.
+#
+# recipe2txt is free software: you can redistribute it and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# recipe2txt is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with recipe2txt.
+# If not, see <https://www.gnu.org/licenses/>.
+
 import logging
 import urllib
 from enum import IntEnum
