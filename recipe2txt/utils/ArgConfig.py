f55b0f8737805cb04c85e8b631934516e365b872
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-09-29 09:35:05 +0200

    Change has_short-argument to short
    This allows setting custom names for the short version of the CLI-flag.

--- recipe2txt/utils/ArgConfig.py
+++ recipe2txt/utils/ArgConfig.py
@@ -12,7 +12,6 @@ from recipe2txt.utils.misc import ensure_accessible_file_critical, File
 
 
 def short_flag(long_name: str) -> str:
-    long_name = long_name.strip()
     segments = long_name.split("-")
     starting_letters = [segment.strip()[0] for segment in segments if segment]
     return "-" + "".join(starting_letters)
@@ -51,14 +50,23 @@ class BasicOption:
                                         break_long_words=False,
                                         break_on_hyphens=False)
 
-    def __init__(self, name: str, help_str: str, default: Any = None, has_short: bool = True):
+    def __init__(self, name: str, help_str: str, default: Any = None, short: str | None = ""):
         name = name.strip()
         if name.startswith('-'):
             raise ValueError("'name' should just be the name of the flag without any leading '-'")
         self.name = name
         self.names = ["--" + name]
-        if has_short:
-            self.names.append(short_flag(name))
+
+        if short is not None:
+            short = short.strip()
+            if short.startswith('-'):
+                raise ValueError("'short' should just be the name of the flag without any leading '-'")
+            if len(short) >= len(name):
+                raise ValueError("'short' is not shorter than 'name'")
+            if short:
+                self.names.append('-' + short)
+            else:
+                self.names.append(short_flag(name))
         self.arguments = {ArgKey.help: help_str, ArgKey.default: default}
 
     def add_to_parser(self, parser: argparse.ArgumentParser) -> None:
@@ -97,10 +105,10 @@ T = TypeVar('T')
 
 class ChoiceOption(BasicOption, Generic[T]):
 
-    def __init__(self, name: str, help_str: str, default: T, choices: Iterable[T], has_short: bool = True):
+    def __init__(self, name: str, help_str: str, default: T, choices: Iterable[T], short: str | None = ""):
         if default not in choices:
             raise ValueError(f"Parameter {default=} not in {choices=}")
-        super().__init__(name, help_str, default, has_short)
+        super().__init__(name, help_str, default, short)
         self.arguments[ArgKey.choices] = choices
 
     def to_toml_str(self) -> str:
@@ -115,10 +123,10 @@ class ChoiceOption(BasicOption, Generic[T]):
 
 class TypeOption(BasicOption):
 
-    def __init__(self, name: str, help_str: str, default: Any, t: type, has_short: bool = True):
+    def __init__(self, name: str, help_str: str, default: Any, t: type, short: str | None = ""):
         if not isinstance(default, t):
             raise ValueError("Parameter {default=} does not match type {t=}")
-        super().__init__(name, help_str, default, has_short)
+        super().__init__(name, help_str, default, short)
         self.arguments[ArgKey.type] = t
 
     def toml_valid(self, value: Any) -> bool:
@@ -129,8 +137,8 @@ class TypeOption(BasicOption):
 
 class BoolOption(BasicOption):
 
-    def __init__(self, name: str, help_str: str, default: bool = False, has_short: bool = True):
-        super().__init__(name, help_str, default, has_short)
+    def __init__(self, name: str, help_str: str, default: bool = False, short: str | None = ""):
+        super().__init__(name, help_str, default, short)
         self.arguments[ArgKey.action] = "store_true"
 
     def to_toml_str(self) -> str:
@@ -144,9 +152,9 @@ class BoolOption(BasicOption):
 
 class NArgOption(BasicOption):
 
-    def __init__(self, name: str, help_str: str, default: list[Any] | None = None, has_short: bool = True):
+    def __init__(self, name: str, help_str: str, default: list[Any] | None = None, short: str | None = ""):
         d = [] if default is None else default
-        super().__init__(name, help_str, d, has_short)
+        super().__init__(name, help_str, d, short)
         self.arguments[ArgKey.nargs] = '+'
 
     def toml_valid(self, value: Any) -> bool:
@@ -204,17 +212,17 @@ class ArgConfig:
             option.to_toml(self.config_file)
         option.add_to_parser(self.parser)
 
-    def add_arg(self, name: str, help_str: str, default: Any = None, has_short: bool = True) -> None:
-        self.add_option(BasicOption(name, help_str, default, has_short))
+    def add_arg(self, name: str, help_str: str, default: Any = None, short: str | None = "") -> None:
+        self.add_option(BasicOption(name, help_str, default, short))
 
-    def add_choice(self, name: str, help_str: str, default: T, choices: Iterable[T], has_short: bool = True) -> None:
-        self.add_option(ChoiceOption(name, help_str, default, choices, has_short))
+    def add_choice(self, name: str, help_str: str, default: T, choices: Iterable[T], short: str | None = "") -> None:
+        self.add_option(ChoiceOption(name, help_str, default, choices, short))
 
-    def add_type(self, name: str, help_str: str, default: Any, t: type, has_short: bool = True) -> None:
-        self.add_option(TypeOption(name, help_str, default, t, has_short))
+    def add_type(self, name: str, help_str: str, default: Any, t: type, short: str | None = "") -> None:
+        self.add_option(TypeOption(name, help_str, default, t, short))
 
-    def add_bool(self, name: str, help_str: str, default: bool = False, has_short: bool = True) -> None:
-        self.add_option(BoolOption(name, help_str, default, has_short))
+    def add_bool(self, name: str, help_str: str, default: bool = False, short: str | None = "") -> None:
+        self.add_option(BoolOption(name, help_str, default, short))
 
-    def add_narg(self, name: str, help_str: str, default: list[Any] | None = None, has_short: bool = True) -> None:
-        self.add_option(NArgOption(name, help_str, default, has_short))
+    def add_narg(self, name: str, help_str: str, default: list[Any] | None = None, short: str | None = "") -> None:
+        self.add_option(NArgOption(name, help_str, default, short))
