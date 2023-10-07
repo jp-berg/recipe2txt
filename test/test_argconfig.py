bb68e2f7c570a179936231067491751d24824d10
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-10-07 21:01:16 +0200

    Extend tests

--- test/test_argconfig.py
+++ test/test_argconfig.py
@@ -40,7 +40,9 @@ class TestFunctions(unittest.TestCase):
                   ([1, 2, 3], "[1, 2, 3]"),
                   (["one", "two", "three"], "['one', 'two', 'three']"),
                   ({"four": True, "five": False, "six": True},
-                   "{'four': true, 'five': false, 'six': true}")]
+                   "{'four': true, 'five': false, 'six': true}"),
+                  ([], "[]"),
+                  ({}, "{}")]
 
         assertEval(self, argconfig.obj2toml, params)
 
@@ -121,11 +123,6 @@ class TestBasicOption(TestInit):
                 b = argconfig.BasicOption(**init_params)
                 self.assertValidInit(b, init_params)
 
-    def test_to_toml_str(self):
-        for idx, (init_params, valid_string) in enumerate(params):
-            with self.subTest(i=idx, parameter=init_params):
-                b = argconfig.BasicOption(**init_params)
-                self.assertEqual(b.to_toml_str(), valid_string)
 
     def test_to_toml(self):
         self.assertIsNotNone(ensure_existence_dir(test_project_tmpdir))
@@ -215,14 +212,8 @@ class TestChoiceOption(TestInitOption):
                     faulty_params = init_params | {"default": valid_string}
                     c = argconfig.ChoiceOption(**faulty_params)
 
-    def test_to_toml_str(self):
-        for idx, (init_params, valid_string) in enumerate(co_params):
-            with self.subTest(i=idx, parameter=init_params):
-                c = argconfig.ChoiceOption(**init_params)
-                self.assertEqual(c.to_toml_str(), valid_string)
-
     def test_toml_valid(self):
-        for idx, (init_params, valid_string) in enumerate(co_params):
+        for idx, (init_params, _) in enumerate(co_params):
             with self.subTest(i=idx, parameter=init_params):
                 c = argconfig.ChoiceOption(**init_params)
                 for choice in init_params["choices"]:
@@ -248,7 +239,9 @@ t_valid_string_2 = textwrap.dedent(
     """
     
     
-    # How many times should the program retry to fetch the resource on failure
+    # How many times should the program retry to fetch the resource on
+    # failure
+    
     #retries = 3
     """
 )
@@ -257,6 +250,7 @@ t_params_2 = {"name": "retries", "help_str": "How many times should the program 
 
 t_params = [(t_params_1, t_valid_string_1),
             (t_params_2, t_valid_string_2)]
+
 
 class TestInitType(TestInit):
     def assertValidInit(self, option: argconfig.BasicOption, validation: dict[str, Any]):
@@ -279,6 +273,134 @@ class TestTypeOption(TestInitType):
                 with self.assertRaises(ValueError):
                     p_invalid = init_params | {"default": "Not a good value"}
                     t = argconfig.TypeOption(**p_invalid)
+
+
+b_valid_string_1 = textwrap.dedent(
+    """
+
+        
+    # Whether the inputs should be validated before processing
+    
+    #validate-input = true # Possible values: true | false
+    """)
+b_params_1 = {"name": "validate-input", "help_str": "Whether the inputs should be validated before processing",
+              "default": True, "short": "v"}
+
+b_valid_string_2 = textwrap.dedent(
+    """
+    
+    
+    # Toggles debug-mode
+    
+    #debug = false # Possible values: true | false
+    """
+)
+
+b_params_2 = {"name": "debug", "help_str": "Toggles debug-mode", "default": False}
+
+b_params = [(b_params_1, b_valid_string_1),
+            (b_params_2, b_valid_string_2)]
+
+
+class TestInitBool(TestInit):
+
+    def assertValidInit(self, option: argconfig.BasicOption, validation: dict[str, Any]):
+        super().assertValidInit(option, validation)
+        self.assertEqual(option.arguments[argconfig.ArgKey.action], "store_true")
+
+
+class BoolOption(TestInitBool):
+
+    def test_init(self):
+        for idx, (init_params, _) in enumerate(b_params):
+            with self.subTest(i=idx, parameter=init_params):
+                b = argconfig.BoolOption(**init_params)
+                self.assertValidInit(b, init_params)
+
+
+n_valid_string_1 = textwrap.dedent(
+    """
+    
+    
+    # List of the ignored directories
+    
+    #ignored-directories = []
+    """
+)
+n_params_1 = {"name": "ignored-directories", "help_str": "List of the ignored directories", "short": "i"}
+
+n_valid_string_2 = textwrap.dedent(
+    """
+    
+    
+    # Values used as test-inputs
+    
+    #testvalues = [37, 29, 54, 66, 19, 59, 32]
+    """
+)
+n_params_2 = {"name": "testvalues", "help_str": "Values used as test-inputs",
+              "default": [37, 29, 54, 66, 19, 59, 32]}
+
+n_valid_string_3 = textwrap.dedent(
+    """
+    
+    
+    # Phrases that will be blocked
+    
+    #banned = ['duck you', 'heck', 'darn']
+    """
+)
+n_params_3 = {"name": "banned", "help_str": "Phrases that will be blocked",
+              "default": ["duck you", "heck", "darn"]}
+
+n_params = [(n_params_1, n_valid_string_1),
+            (n_params_2, n_valid_string_2),
+            (n_params_3, n_valid_string_3)]
+
+
+class TestInitNArg(TestInit):
+
+    def assertValidInit(self, option: argconfig.BasicOption, validation: dict[str, Any]):
+        super().assertValidInit(option, validation)
+        self.assertEqual(option.arguments[argconfig.ArgKey.nargs], "+")
+
+
+class NArgOption(TestInitNArg):
+
+    def test_init(self):
+        for idx, (init_params, _) in enumerate(n_params):
+            with self.subTest(i=idx, parameter=init_params):
+                n = argconfig.NArgOption(**init_params)
+                if "default" not in init_params:
+                    init_params |= {"default": []}
+                self.assertValidInit(n, init_params)
+
+
+all_options = [(argconfig.BasicOption(**params), valid_string) for params, valid_string in params]
+all_options += [(argconfig.ChoiceOption(**params), valid_string) for params, valid_string in co_params]
+all_options += [(argconfig.TypeOption(**params), valid_string) for params, valid_string in t_params]
+all_options += [(argconfig.BoolOption(**params), valid_string) for params, valid_string in b_params]
+all_options += [(argconfig.NArgOption(**params), valid_string) for params, valid_string in n_params]
+
+
+class TestArgConfig(unittest.TestCase):
+
+    def test_to_toml_str(self):
+        for idx, (option, valid_string) in enumerate(all_options):
+            with self.subTest(i=idx):
+                self.assertEqual(option.to_toml_str(), valid_string)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
