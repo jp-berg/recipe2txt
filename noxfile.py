4adf4a7b540c78cd879af098197031c7756acb47
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-10-25 18:03:17 +0200

    Reformat the noxfile with black

--- noxfile.py
+++ noxfile.py
@@ -13,10 +13,10 @@ root_env = {"PYTHONPATH": str(root)}
 nox.options.envdir = "/tmp/nox"
 
 pyproject_toml = "pyproject.toml"
-VENV_DIR = Path('./.venv').resolve()
+VENV_DIR = Path("./.venv").resolve()
 python = str(VENV_DIR / "bin" / "python")
 
-PYTHON_VERSIONS = ['3.8', '3.9', '3.10', '3.11']
+PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11"]
 
 
 @cache
@@ -59,13 +59,20 @@ def get_packages(session: nox.Session) -> tuple[Path, Path]:
 def install_deps(session: nox.Session, python_path: str = "python") -> None:
     if not (dependencies := get_deps()):
         session.error(pyproject_toml + " does not exist")
-    to_install = [f"{name}{constraint.replace(' ', '')}" for name, constraint in dependencies
-                  if not is_available(session, name, python_path)]
+    to_install = [
+        f"{name}{constraint.replace(' ', '')}"
+        for name, constraint in dependencies
+        if not is_available(session, name, python_path)
+    ]
     if to_install:
-        session.run(python_path, "-m", "pip", "install", *to_install, external=True, silent=True)
+        session.run(
+            python_path, "-m", "pip", "install", *to_install, external=True, silent=True
+        )
 
 
-def run(session: nox.Session, program: str, *args: str, use_root_env: bool = False) -> None:
+def run(
+    session: nox.Session, program: str, *args: str, use_root_env: bool = False
+) -> None:
     if not which(program):
         session.install(program)
     if use_root_env:
@@ -74,17 +81,31 @@ def run(session: nox.Session, program: str, *args: str, use_root_env: bool = Fal
         session.run(program, *args, external=True)
 
 
-def is_available(session: nox.Session, package_name: str, python_path: str = "python3",) -> bool:
-    out = session.run(python_path, "-c",
-                      f"import {package_name.replace('-', '_')}", success_codes=[0, 1], external=True, silent=True)
+def is_available(
+    session: nox.Session,
+    package_name: str,
+    python_path: str = "python3",
+) -> bool:
+    out = session.run(
+        python_path,
+        "-c",
+        f"import {package_name.replace('-', '_')}",
+        success_codes=[0, 1],
+        external=True,
+        silent=True,
+    )
     return False if out else True
 
 
 def is_applicable(session: nox.Session, constraints: str) -> bool:
-    constraints_list = [[component for component in con.split(" ") if component] for con in constraints.split(";") if con][1:]
+    constraints_list = [
+        [component for component in con.split(" ") if component]
+        for con in constraints.split(";")
+        if con
+    ][1:]
     if not constraints_list:
         return True
-    if not(out := session.run("python", "--version", silent = True)):
+    if not (out := session.run("python", "--version", silent=True)):
         session.error("python not found")
     _, py_vers = out.split(" ")
     py_vers = py_vers.strip()
@@ -92,11 +113,11 @@ def is_applicable(session: nox.Session, constraints: str) -> bool:
         if not con:
             continue
         if con[0] == ("python_version"):
-            if con[1] in '=>=<=':
+            if con[1] in "=>=<=":
                 return bool(eval(f"'{py_vers}' {con[1]} {con[2]}"))
             else:
                 return False
-    session.error("Constraints unclear: "+ constraints)
+    session.error("Constraints unclear: " + constraints)
 
 
 @nox.session(reuse_venv=True)
@@ -105,7 +126,9 @@ def dev(session: nox.Session) -> None:
     if not VENV_DIR.is_dir():
         run(session, "virtualenv", str(VENV_DIR))
     install_deps(session, python)
-    session.run(python, "-m", "pip", "install", "nox", external=True, silent=True) #so mypy can typecheck this file
+    session.run(
+        python, "-m", "pip", "install", "nox", external=True, silent=True
+    )  # so mypy can typecheck this file
     session.notify("check")
 
 
@@ -113,7 +136,6 @@ def dev(session: nox.Session) -> None:
 def check(session: nox.Session) -> None:
     """Uses static code analysis and runs unittests."""
     run(session, "mypy")
-    run(session, "isort", "recipe2txt", "test")
     session.run(python, "-m", "unittest", external=True)
 
 
@@ -127,7 +149,7 @@ def tidy(session: nox.Session) -> None:
 @nox.session(reuse_venv=True)
 def short(session: nox.Session) -> None:
     """System-testing: A short run of the entire program."""
-    #session.notify("check")
+    # session.notify("check")
     install_deps(session)
     session.run("python", "-m", "unittest", "discover", "-s", "test")
     session.run("python", "-m", "test.test4recipe2txt", env=root_env)
@@ -153,8 +175,16 @@ def all(session: nox.Session) -> None:
     """System-testing: Scrape all URLs at once."""
     session.notify("check")
     install_deps(session)
-    session.run("python", "-m", "test.test4recipe2txt", "--delete-database",
-                "--long-timeout", "--number-of-urls", "-1", env=root_env)
+    session.run(
+        "python",
+        "-m",
+        "test.test4recipe2txt",
+        "--delete-database",
+        "--long-timeout",
+        "--number-of-urls",
+        "-1",
+        env=root_env,
+    )
 
 
 @nox.session(reuse_venv=True)
@@ -179,7 +209,7 @@ def clean(session: nox.Session) -> None:
         root / ".mypy_cache",
         root / "test" / "testfiles" / "tmp_testfiles_re2txt",
         root / "test" / "reports_test4recipe2txt",
-        root / "recipe2txt.egg-info"
+        root / "recipe2txt.egg-info",
     ]
     for p in to_remove:
         session.log(f"Removing{p}")
@@ -194,9 +224,3 @@ def uninstall(session: nox.Session) -> None:
     """Uses 'clean' and removes the dev-virtual-environment."""
     session.notify("clean")
     rmtree(VENV_DIR, ignore_errors=True)
-
-
-
-
-
-
