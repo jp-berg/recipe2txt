9494e608bf31abb9577bd30e476e6d6b7643ae60
Jan Philipp Berg <git.7ksst@aleeas.com>
2024-02-11 15:32:48 +0100

    Add markdown-tests

--- test/test_recipes2out.py
+++ test/test_recipes2out.py
@@ -16,13 +16,20 @@
 
 import unittest
 from test.test_helpers import TEST_PROJECT_TMPDIR, TESTFILE, delete_tmpdirs
-from test.testfiles.permanent.testfile_generator import FULL_TXT, RECIPE_LIST, TXT_LIST
+from test.testfiles.permanent.testfile_generator import (
+    FULL_MD,
+    FULL_TXT,
+    MD_LIST,
+    RECIPE_LIST,
+    TXT_LIST,
+)
 
 from recipe2txt.recipes2out import RecipeWriter
 from recipe2txt.utils.ContextLogger import suppress_logging
 from recipe2txt.utils.misc import ensure_accessible_file_critical
 
-TESTFILE_TXT = TEST_PROJECT_TMPDIR / TESTFILE
+
+TESTFILE_MD = TESTFILE.replace("txt", "md")
 
 
 class Test(unittest.TestCase):
@@ -52,3 +59,26 @@ class Test(unittest.TestCase):
 
         test_recipes = file.read_text()
         self.assertEqual(test_recipes, FULL_TXT)
+
+    def test_RecipeWriter_single_recipe_md(self):
+
+        for recipe, validation_recipe in zip(RECIPE_LIST, MD_LIST):
+            with self.subTest(recipe=recipe.title):
+                file = ensure_accessible_file_critical(TEST_PROJECT_TMPDIR, TESTFILE_MD)
+                with suppress_logging():
+                    txt_writer = RecipeWriter(file, "md", True)
+                    txt_writer.write([recipe])
+
+                test_recipe = file.read_text()
+                self.assertEqual(test_recipe, validation_recipe)
+
+                delete_tmpdirs()
+
+    def test_RecipeWriter_multiple_recipes_md(self):
+        file = ensure_accessible_file_critical(TEST_PROJECT_TMPDIR, TESTFILE_MD)
+        with suppress_logging():
+            txt_writer = RecipeWriter(file, "md", True)
+            txt_writer.write(RECIPE_LIST)
+
+        test_recipes = file.read_text()
+        self.assertEqual(test_recipes, FULL_MD)
